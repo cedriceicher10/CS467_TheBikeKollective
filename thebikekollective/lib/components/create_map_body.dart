572a4961208ecbc0a7941eef306b92e90edefcc5
drab <58239644+pollocco@users.noreply.github.com>
2021-10-27 17:13:37 -0500

    Update create_map_body.dart

--- thebikekollective/lib/components/create_map_body.dart
+++ thebikekollective/lib/components/create_map_body.dart
@@ -54,7 +54,7 @@ class CreateMapBody extends StatefulWidget{
 }
 
 
-class _CreateMapBody extends State<CreateMapBody>{
+class _CreateMapBody extends State<CreateMapBody> with TickerProviderStateMixin{
     List<BikeMarker> bikes = <BikeMarker>[];
     final PopupController _popupLayerController = PopupController();
 
@@ -68,6 +68,43 @@ class _CreateMapBody extends State<CreateMapBody>{
     void _zoom() {
       currentZoom = currentZoom - 1;
       mapController!.move(currentCenter, currentZoom);
+    }
+
+    void _animatedMapMove(LatLng destLocation, double destZoom) {
+      // Create some tweens. These serve to split up the transition from one location to another.
+      // In our case, we want to split the transition be<tween> our current map center and the destination.
+      final _latTween = Tween<double>(
+          begin: mapController!.center.latitude, end: destLocation.latitude);
+      final _lngTween = Tween<double>(
+          begin: mapController!.center.longitude, end: destLocation.longitude);
+      final _zoomTween = Tween<double>(begin: mapController!.zoom, end: destZoom);
+
+      // Create a animation controller that has a duration and a TickerProvider.
+      var controller = AnimationController(
+          duration: const Duration(milliseconds: 500), vsync: this);
+      // The animation determines what path the animation will take. You can try different Curves values, although I found
+      // fastOutSlowIn to be my favorite.
+      Animation<double> animation =
+      CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn);
+
+      controller.addListener(() {
+        mapController!.move(
+            LatLng(_latTween.evaluate(animation), _lngTween.evaluate(animation)),
+            _zoomTween.evaluate(animation));
+      });
+
+      animation.addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.dispose();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.dispose();
+        }
+      });
+
+      currentZoom = destZoom;
+      currentCenter = destLocation;
+
+      controller.forward();
     }
 
     @override
@@ -94,6 +131,8 @@ class _CreateMapBody extends State<CreateMapBody>{
                       },
                       center: currentCenter,
                       zoom: currentZoom,
+                      // Conzar! You can disable rotation using this,
+                      // but try not to take the easy way out:
                       interactiveFlags: InteractiveFlag.all,
                       onTap: (a, b)
                       {
@@ -133,6 +172,7 @@ class _CreateMapBody extends State<CreateMapBody>{
                       markerTapBehavior: MarkerTapBehavior.custom((marker, popupController) => {
                         popupController.hideAllPopups(),
                         popupController.togglePopup(marker),
+                        _animatedMapMove(marker.point, 17)
                       }
                       )
                     ),
@@ -246,7 +286,7 @@ Container portraitLayout(BuildContext context, bike){
                         SizedBox(height: 8),
                         Text('Condition: ${bike.condition}'),
                         SizedBox(height: 8),
-                        rideButton(context, "Ride me!", 100, 25)
+                        rideButton(context, "Details", 100, 25)
                       ])
                 ]
             )
